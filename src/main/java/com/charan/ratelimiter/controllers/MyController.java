8de9e506eccc40e87042b5776c12c8c3fdd4ea7f
Sri Charan Dutta Siram <cherrysri1997@gmail.com>
2021-11-16 19:25:17 +0530

    Final Commit.

--- src/main/java/com/charan/ratelimiter/controllers/MyController.java
+++ src/main/java/com/charan/ratelimiter/controllers/MyController.java
@@ -25,36 +25,4 @@ public class MyController {
                 + " and number of tokens available for you are: "
                 + availableTokens;
     }
-
-//    private final Bucket bucket;
-
-//    public MyController() {
-//        int capacity = 50;
-//        Refill refill = Refill.intervally(capacity, Duration.ofMinutes(60));
-//        Bandwidth limit = Bandwidth.classic(capacity, refill);
-//        this.bucket = Bucket4j
-//                .builder()
-//                .addLimit(limit)
-//                .build();
-//        System.out.println(this.bucket.getAvailableTokens());
-//
-//    }
-
-//    @GetMapping("/getRateLimitedData")
-//    public ResponseEntity<String> getRateLimitedData() {
-//        ConsumptionProbe consumptionProbe = this.bucket.tryConsumeAndReturnRemaining(1);
-//        if(consumptionProbe.isConsumed()) {
-//            return ResponseEntity
-//                    .ok()
-//                    .header("X-Rate-Limit-Remaining", Long.toString(consumptionProbe.getRemainingTokens()))
-//                    .body("Number of tokens remaining are: " + consumptionProbe.getRemainingTokens());
-//        }
-//        return ResponseEntity
-//                .status(HttpStatus.TOO_MANY_REQUESTS)
-//                .header("X-Rate-Limit-Retry-After-Seconds", Long.toString(
-//                        TimeUnit.NANOSECONDS.toMillis(consumptionProbe.getNanosToWaitForRefill())/1000
-//                    )
-//                )
-//                .build();
-//    }
 }
